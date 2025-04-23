6468f9216b3c76eb84d9cb949d9a74d1e619a7c9
tomglencross69 <thomasglencross@gmail.com>
2025-04-23 14:47:30 +0100

    builds scroller

--- components/ScrollHeader.vue
+++ components/ScrollHeader.vue
@@ -1,43 +1,40 @@
 <template>
     <div class="scroll-container">
-        <div class="scroll-text">
-            <div class="scroll-inner">
-            <span>This is the scrolling text. </span>
-            <span>This is the scrolling text.</span>
-            <span>This is the scrolling text.</span>
+      <div class="scroll-text">
+        <div class="scroll-inner">
+          <span v-for="n in 10" :key="n">no zuck no ads no doomscroll no endless feed no addiction no problem&#x00A0</span>
         </div>
-        </div>
+      </div>
     </div>
-</template>
-
+  </template>
+  
 <script setup>
 
 </script>
 
-<style lang="css" scoped>
-
+<style scoped lang="css">
 .scroll-container {
-border: 1px solid black;
-overflow: hidden;
+  overflow: hidden;
+  border: 1px solid black;
 }
 
 .scroll-text {
-    display: flex;
-    white-space: nowrap;
+  display: flex;
+  white-space: nowrap;
 }
 
 .scroll-inner {
-    display: inline-flex;
-    animation: my-animation 10s linear infinite;
+ font-family: 'MyCustomGothicFont', serif;
+  display: inline-flex;
+  animation: scroll-loop 30s linear infinite;
 }
 
-@keyframes my-animation {
+@keyframes scroll-loop {
   from {
     transform: translateX(0%);
   }
   to {
-    transform: translateX(-50%);
+    transform: translateX(-60%);
   }
 }
-
 </style>
