5e76061b5d0bf832b256bca72d6bbfef57e84755
tomglencross69 <thomasglencross@gmail.com>
2025-05-07 17:15:25 +0100

    adds small corrections in seed

--- db/test-connection.ts
+++ db/test-connection.ts
@@ -7,4 +7,5 @@ const db = new Pool({
     connectionString: process.env.TEST_DATABASE_URL,
 })
 
+console.log("Log in connection: Using database:", process.env.TEST_DATABASE_URL);
 export default db
